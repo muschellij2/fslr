72267ba19f6daafdf6e3e7775cdd085f54933fa2
muschellij2 <muschellij2@gmail.com>
2014-09-25 15:42:24 -0400

    So it's \code{\link} not other way around.

--- R/robust_window.R
+++ R/robust_window.R
@@ -3,16 +3,16 @@
 #' @description Takes an image, finds the quantiles given by 
 #' \code{probs}, then sets values outside these values to other 
 #' values, as determined by \code{replace} argument passed to
-#' to \link{\code{window_img}}
+#' to \code{\link{window_img}}
 #' @param img object of class nifti
-#' @param probs quantiles to constrain the image these define the window sent to \link{\code{window_img}}
-#' @param ... additional arguments sent to \link{\code{window_img}}
+#' @param probs quantiles to constrain the image these define the window sent to \code{\link{window_img}}
+#' @param ... additional arguments sent to \code{\link{window_img}}
 #' @export
 #' @return Object of class nifti with values outside quantiles replaced
-#' by values depending on replace argument passed to \link{\code{window_img}}
+#' by values depending on replace argument passed to \code{\link{window_img}}
 robust_window <- function(img, # object of class nifti
-           probs = c(0, 0.999), # quantiles to constrain the image, these define the window sent to \link{\code{window_img}}
-           ... # additional arguments sent to \link{\code{window_img}}
+           probs = c(0, 0.999), # quantiles to constrain the image, these define the window sent to \code{\link{window_img}}
+           ... # additional arguments sent to \code{\link{window_img}}
            ){
   cc = c(img)
   quant = quantile(cc, probs = probs)
