2fa745216829b28e37c003cca4fbf968f5204242
muschellij2 <muschellij2@gmail.com>
2014-10-01 01:20:04 -0400

    Added onefile function.

--- R/nii.stub.R
+++ R/nii.stub.R
@@ -81,3 +81,15 @@ drop_img_dim = function(img){
   img
 }
 
+#' @title Creates onefile specification
+#' @return Object of class nifti
+#' @param img nifti object
+#' @description Changes the magic and vox_offset slots to be consistent 
+#' with onefile option in \code{\link{writeNIfTI}}.  As of version 0.4.0,
+#' \code{oro.nifti} did not support "ni1" magic outputs for output.
+#' @export
+onefile = function(img){
+  img@magic = "n+1"
+  img@vox_offset = 352
+  img
+}
