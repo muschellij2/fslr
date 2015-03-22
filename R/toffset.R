8f74363c2de9c7eaa7c2f72bc4bdd9bff513d33d
muschellij2 <muschellij2@gmail.com>
2015-03-22 18:11:58 -0400

    fixed character versions of S4 functions

--- R/toffset.R
+++ R/toffset.R
@@ -1,3 +1,10 @@
+#' @docType methods
+#' @rdname toffset-methods
+#' @title Extract Image toffset attribute 
+#' @name toffset-methods
+#' @aliases toffset,character-method
+#' @param object is a filename to pass to \link{fslval}
+#' @import oro.nifti
 #' @export
 setMethod("toffset", "character", function(object) { 
   object = path.expand(object)
