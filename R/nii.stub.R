1c1cc62b84176848a60a813d90501632f6ecd603
muschellij2 <muschellij2@gmail.com>
2015-04-15 02:29:09 -0400

    Fixed nii.stub so names would carry over.

--- R/nii.stub.R
+++ R/nii.stub.R
@@ -6,9 +6,11 @@
 #' @param bn Take \code{\link{basename}} of file?
 #' @export
 nii.stub = function(x, bn=FALSE){
+  nx = names(x)
   x = path.expand(x)
   stub = gsub("\\.gz$", "", x)
   stub = gsub("\\.nii$", "", stub)
   if (bn) stub = basename(stub)
+  names(stub) = nx
   return(stub)
 }
