c3f498776b44d0c334f17e3ed97bc43e1db17085
muschellij2 <muschellij2@gmail.com>
2015-11-24 15:26:44 -0500

    Adding replace_dropped_dimensions

---
+++ R/replace_dropped_dimensions.R
@@ -0,0 +1,21 @@
+#' @title Remake Dropped Dimensions
+#' @description This funtion is the reverse of \code{dropEmptyImageDimensions}.
+#' If \code{dropEmptyImageDimensions} was run, and the output is a list, 
+#' usually if \code{keep_ind = TRUE}, this function reverses that
+#'
+#' @param img Object of class \code{nifti}
+#' @param inds List of length 3 of indices from 
+#' \code{\link{dropEmptyImageDimensions}}
+#' @param orig.dim Original dimension of pre-dropped image. Output image will
+#' have dimensions same as this
+#'
+#' @return Object of class nifti
+#' @export
+replace_dropped_dimensions = function(img, inds, orig.dim){
+  stopifnot(length(orig.dim) == 3)
+  arr = array(0, dim = orig.dim)
+  arr[inds[[1]], inds[[2]], inds[[3]]] = img
+  outimg = copyNIfTIHeader(img = img, 
+                           arr = arr, 
+                           drop = TRUE)
+}
