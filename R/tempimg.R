ca011dfcca2c3fe7cec037cca1d12dc95a4e0b1b
muschellij2 <muschellij2@gmail.com>
2015-12-01 16:10:18 -0500

    Using writenii rather than writeNIfTI for tempimg

--- R/tempimg.R
+++ R/tempimg.R
@@ -7,26 +7,32 @@
 #' \code{\link{writeNIfTI}}
 #' @param checknan Check for NAs or NaNs
 #' @param check_type Check the datatype for an image.  Will run \code{\link{datatyper}}.
+#' @param warn Should warnings be displayed if \code{\link{writeNIfTI}} has
+#' any?  Passed to \code{\link{writenii}}.
 #' @param ... Not used
 #' @return filename of output nii.gz
 #' @export
-tempimg = function(nim, gzipped= TRUE, checknan = TRUE, check_type = FALSE, ...){
+tempimg = function(nim, gzipped = TRUE, checknan = TRUE, 
+                   check_type = FALSE, warn = FALSE, 
+                   ...){
   f = tempfile()
   nim = cal_img(nim)
   nim = zero_trans(nim)
-  if (checknan){
+  if (checknan) {
     cnim = c(nim)
-    if (any(is.na(cnim) | is.nan(cnim))){
+    if (any(is.na(cnim) | is.nan(cnim))) {
       warning("NAs and NaNs in image file, replacing with zeros")
-      nim[is.na(nim)| is.nan(cnim)] = 0
+      nim[is.na(nim) | is.nan(cnim)] = 0
     }
   }
-  if (check_type){
+  if (check_type) {
     nim = datatyper(nim)
   }
   ########### added for weird stuff of NIfTI
   nim = drop_img_dim(nim)
-  writeNIfTI(nim, filename= f, onefile = TRUE, gzipped = gzipped)
+  writenii(nim, filename = f, 
+           onefile = TRUE, gzipped = gzipped,
+           warn = warn)
   ext = ".nii"
   if (gzipped) ext = paste0(ext, '.gz')
   f = paste0(f, ext)
