abf308c92331778c9617f65756040d83aeb5715b
muschellij2 <muschellij2@gmail.com>
2015-12-01 16:04:55 -0500

    Added warning option to writenii.

--- R/writenii.R
+++ R/writenii.R
@@ -1,7 +1,8 @@
 #' @title writeNIfTI with default non-reoirientation
 #' 
 #' @description  This function calls the \code{\link{writeNIfTI}} function from the 
-#' \code{oro.nifti} package, but makes sure to remove \code{.nii} extension
+#' \code{oro.nifti} package, but makes sure to remove \code{.nii} extension and
+#' warnings can be suppressed.
 #' @param nim object of class \code{nifti}, passed to \code{\link{writeNIfTI}}
 #' @param filename path to save the NIfTI file.  Suffix will be removed
 #' @param dtype Should \code{\link{datatyper}} be run before writing?
@@ -13,7 +14,7 @@
 #' @rdname writeNIfTI2
 #' @export
 writeNIfTI2 <- function(nim, filename, dtype = FALSE, ...){
-  if (dtype){
+  if (dtype) {
     nim = datatyper(nim)
   }
   oro.nifti::writeNIfTI(nim, nii.stub(filename), ...)
@@ -21,15 +22,25 @@ writeNIfTI2 <- function(nim, filename, dtype = FALSE, ...){
 
 #' @rdname writeNIfTI2
 #' @param drop_dim Should \code{\link{drop_img_dim}} be run before writing?
+#' @param warn Should warnings from \code{\link{writeNIfTI}} be 
+#' printed?  If not, \code{\link{suppressWarnings}} is called 
 #' @export
 writenii <- function(nim, filename, 
-                     dtype = TRUE, drop_dim = TRUE, ...){
+                     dtype = TRUE, 
+                     drop_dim = TRUE, 
+                     warn = FALSE, ...){
   if (drop_dim) {
     nim = drop_img_dim(nim)
   }
   if (dtype) {
     nim = datatyper(nim)
   }
-  
-  oro.nifti::writeNIfTI(nim, nii.stub(filename), ...)
+  if (warn) {
+    x = oro.nifti::writeNIfTI(nim, nii.stub(filename), ...)
+  } else {
+    suppressWarnings({
+      x = oro.nifti::writeNIfTI(nim, nii.stub(filename), ...)
+    })
+  }
+  return(x)
 }
