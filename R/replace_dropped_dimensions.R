c903bec07dd2ecbfa62c0fcfd8ccfe182b8f07c5
muschellij2 <muschellij2@gmail.com>
2015-11-30 19:05:22 -0500

    Added example to replace_dropped_dimensions.

--- R/replace_dropped_dimensions.R
+++ R/replace_dropped_dimensions.R
@@ -9,8 +9,16 @@
 #' @param orig.dim Original dimension of pre-dropped image. Output image will
 #' have dimensions same as this
 #'
-#' @return Object of class nifti
+#' @return Object of class \code{\link{nifti}}
 #' @export
+#' @examples \dontrun{
+#' # nim is an object of class nifti
+#' dd = dropEmptyImageDimensions(nim, keep_ind = TRUE)
+#' remake = replace_dropped_dimensions(img = dd$outimg, 
+#' inds = dd$inds,
+#' orig.dim = dd$orig.dim)
+#' all.equal(nim, remake)
+#' }
 replace_dropped_dimensions = function(img, inds, orig.dim){
   stopifnot(length(orig.dim) == 3)
   arr = array(0, dim = orig.dim)
