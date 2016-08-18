6442487f4f9407a672bd7f19bbd3a329a3b4ad36
muschellij2 <muschellij2@gmail.com>
2016-08-18 14:52:43 -0400

    added eddy_current

---
+++ R/eddy_correct.R
@@ -0,0 +1,27 @@
+#' @title Eddy Current Correction
+#' @description This function calls \code{eddy_correct} from FSL for 
+#' DTI Processing
+#' @param infile input filename of 4D image.
+#' @param outfile Output filename 
+#' @param retimg (logical) return image of class nifti
+#' @param reference_no Set the volume number for the reference volume 
+#' that will be used as a target to register all other volumes to. 
+#' (default=0, i.e. the first volume)
+#'
+#' @return If \code{retimg} then object of class nifti.  Otherwise,
+#' Result from system command, depends if intern is TRUE or FALSE.
+#' @export
+eddy_correct = function(infile, 
+                        outfile = NULL, 
+                        retimg = TRUE, 
+                        reference_no = 0, ...) {
+  fslcmd(
+    func = "eddy_correct",
+    file = infile,
+    outfile = outfile, 
+    retimg = retimg,
+    opts = reference_no,
+    opts_after_outfile = TRUE,
+    ...)
+    
+}
