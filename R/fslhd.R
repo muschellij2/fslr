663e9a5e5625dfb804777979279821145364709d
muschellij2 <muschellij2@gmail.com>
2016-09-20 17:03:57 -0400

    Updated to 2.0! Neurobase is a main dep now!

--- R/fslhd.R
+++ R/fslhd.R
@@ -10,6 +10,7 @@
 #' \code{getOption("fsl.path")}, it will try the default directory \code{/usr/local/fsl}.
 #' @return NULL if FSL in path, or bash code for setting up FSL DIR
 #' @export
+#' @import neurobase
 get.fsl = function(add_bin = TRUE){
   cmd = NULL
   fsldir = Sys.getenv("FSLDIR")
@@ -1220,54 +1221,6 @@ fslbet.help = function(betcmd = c("bet2", "bet")){
   betcmd = match.arg( betcmd )
   return(fslhelp(betcmd, help.arg = "-h"))
 }
-
-
-#' @title Image Center of Gravity
-#' @description Find Center of Gravity of Image, after thresholding
-#' @param img Object of class nifti
-#' @param thresh threshold for image, will find \code{img > 0}
-#' @param ceil Run \code{\link{ceiling}} to force integers (usu for plotting)
-#' @param warn Produce a warning if the image is empty after thresholding
-#' @return Vector of length 3
-#' @export
-#' @examples
-#' if (have.fsl()){
-#' x = array(rnorm(1e6), dim = c(100, 100, 100))
-#' img = nifti(x, dim= c(100, 100, 100), 
-#' datatype = convert.datatype()$FLOAT32, cal.min = min(x), 
-#' cal.max = max(x), pixdim = rep(1, 4))
-#' cog(img)
-#' } 
-cog = function(img, thresh = 0, ceil = FALSE, warn = TRUE){
-  #   stopifnot(inherits(img, "nifti"))
-  mask = img > thresh
-  if (sum(mask, na.rm = TRUE) == 0) {
-    if (warn) {
-      warning(paste0("No voxels found to be > ", round(thresh, 3), 
-                     ", using the center of whole image"))
-    }
-    xyz = dim(mask) / 2
-  } else {
-    xyz = colMeans(which(mask, arr.ind = TRUE))
-  }
-  if (ceil) xyz = ceiling(xyz)
-  return(xyz)
-}
-
-
-
-#' @title Image Center of Gravity Wrapper
-#' @description Find Center of Gravity of Image, after thresholding and
-#' take ceiling (wrapper for \code{\link{cog}})
-#' @param ... Arguments ppssed to \code{\link{cog}}
-#' @return Vector of length 3
-#' @note Just a convenience wrapper for \code{cog(ceil=TRUE)}
-#' @export
-xyz = function(...){
-  xyz = cog(..., ceil = TRUE)
-  return(xyz)
-}
-
 
 
 #' @title Image Center of Gravity (FSL)
