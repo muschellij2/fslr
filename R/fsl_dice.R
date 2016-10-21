6f9985d3b06407c778e0ee4753308eb06d8c2ed0
muschellij2 <muschellij2@gmail.com>
2016-10-21 15:06:49 -0400

    Moved fsl_dice to own file.

---
+++ R/fsl_dice.R
@@ -0,0 +1,19 @@
+
+#' @title Calculate Dice Coefficient of 2 Binary images
+#'
+#' @description Creates a 2 by 2 table for 
+#' @param x filename of logical or 0/1 image
+#' @param y filename of logical or 0/1 vimage
+#' 
+#' @return Single number of the dice coefficient
+fsl_dice = function(
+  x, y) {
+  
+  tab = fsl_bin_tab(x, y)
+  dtab = dim(tab)
+  stopifnot(all(dtab == c(2,2)))
+  good = 2 * tab[2, 2]
+  dice = good/(good + tab[1, 2] + tab[2, 1])
+  
+  return(dice)
+}
