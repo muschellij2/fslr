fd70dbd461cb1f21e26bf636547866dfd934a4cc
muschellij2 <muschellij2@gmail.com>
2017-09-22 15:17:04 -0400

    Fixing run_first_all

--- R/run_first_all.R
+++ R/run_first_all.R
@@ -8,15 +8,18 @@
 #' @param affine specifies the affine registration matrix to standard space (optional)
 #' @param opts (character) operations to be passed to \code{run_first_all} 
 #' @param verbose (logical) print out command before running
-#' @return Result of \code{\link{system}}
+#' @return List of results, including result of \code{\link{system}}
+#' and some output files
 #' @export
-run_first_all <- function(img, oprefix, 
-                          brain_extracted = FALSE,
-                          structures = NULL,
-                          affine = NULL,
-                          opts = "",
-                          verbose = TRUE
-                          ){
+run_first_all <- function(
+  img, 
+  oprefix = tempfile(), 
+  brain_extracted = FALSE,
+  structures = NULL,
+  affine = NULL,
+  opts = "",
+  verbose = TRUE
+){
   img = checkimg(img)
   args = list(i = shQuote(img),
               o = shQuote(oprefix))
@@ -25,29 +28,72 @@ run_first_all <- function(img, oprefix,
   }
   args$a = affine
   args$structures = structures
+  if (verbose) {
+    args$v = ""
+  }
   
   name_args = names(args)
   if (is.null(name_args)) {
     name_args = rep("", length(args))
   }
+  
   stopifnot(length(name_args) == length(args))
   name_args = paste0("-", name_args)
   args = paste(name_args, args)
   args = paste(args, sep = "", collapse = " ")
   
   cmd = get.fsl()
-
+  
   ##########################
   # Add frontopts
   ##########################
   cmd <- paste0(cmd, "run_first_all ", args)
-
+  
   if (verbose) {
     message(cmd, "\n")
   }
   res = system(cmd, intern = FALSE)
   
-  return(res)
+  L = list(result = res)
+  stubs = c(
+    "_all_fast_firstseg.nii.gz", 
+    "_all_fast_origsegs.nii.gz",
+    "-BrStem_first.bvars", "-BrStem_first.vtk", 
+    "-L_Accu_first.bvars", "-L_Accu_first.vtk", 
+    "-L_Amyg_first.bvars", "-L_Amyg_first.vtk", 
+    "-L_Caud_first.bvars", "-L_Caud_first.vtk", 
+    "-L_Hipp_first.bvars", "-L_Hipp_first.vtk", 
+    "-L_Pall_first.bvars", "-L_Pall_first.vtk", 
+    "-L_Puta_first.bvars", "-L_Puta_first.vtk", 
+    "-L_Thal_first.bvars", "-L_Thal_first.vtk", 
+    "-R_Accu_first.bvars", "-R_Accu_first.vtk", 
+    "-R_Amyg_first.bvars", "-R_Amyg_first.vtk", 
+    "-R_Caud_first.bvars", "-R_Caud_first.vtk", 
+    "-R_Hipp_first.bvars", "-R_Hipp_first.vtk", 
+    "-R_Pall_first.bvars", "-R_Pall_first.vtk", 
+    "-R_Puta_first.bvars", "-R_Puta_first.vtk", 
+    "-R_Thal_first.bvars", "-R_Thal_first.vtk", 
+    ".com", ".com2", ".com3", ".logs")  
+
+  seg = paste0(
+    oprefix, 
+    "_all_fast_firstseg.nii.gz")
+  if (file.exists(seg)) {
+    L$segmentation = seg
+  } 
+  seg_list = paste0(
+    oprefix, 
+    "_all_fast_origsegs.nii.gz")
+  if (file.exists(seg_list)) {
+    L$segmentation_list = seg_list
+  }
+  
+  outfiles = paste0(oprefix, stubs)
+  outfiles = outfiles[ file.exists(outfiles)]
+  
+  L$outfiles = outfiles
+  
+  return(L)
 }
 
 
