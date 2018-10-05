bac3374681d957eb835bd06bef161c8991d2bb52
muschellij2 <muschellij2@gmail.com>
2018-10-05 16:57:54 -0400

    Added deprecation warning to fslview.

--- R/fslview.R
+++ R/fslview.R
@@ -8,6 +8,8 @@
 #' @param ... options passed to \code{\link{checkimg}}
 #' @return character or logical depending on intern
 #' @importFrom utils compareVersion
+#' @note As of FSL version 5.0.10, this is deprecated:
+#' https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/WhatsNew
 #' @export
 fslview = function(file, intern=TRUE, opts ="", verbose = TRUE, ...){
   cmd <- get.fsl()
@@ -35,6 +37,11 @@ fslview = function(file, intern=TRUE, opts ="", verbose = TRUE, ...){
   comp = utils::compareVersion(fslver, "5.0.10")
   if (comp >= 0) {
     fslview_cmd = paste0(fslview_cmd, "_deprecated")
+    .Deprecated(
+      new = "fsleyes", 
+      msg = paste0("fslview is deprecated, calling fslview_deprecated", 
+                   ", but this function may go away")
+    )
   }
   string = paste0(fslview_cmd, " %s")
   cmd <- paste0(cmd, sprintf(string, file))  
@@ -55,7 +62,16 @@ fslview = function(file, intern=TRUE, opts ="", verbose = TRUE, ...){
 #'  fslview.help()
 #' }   
 fslview.help = function(){
-  return(fslhelp("fslview"))
+  fslview_cmd = "fslview"
+  fslver = fsl_version()
+  ################################## 
+  # new version deprecated it
+  ################################## 
+  comp = utils::compareVersion(fslver, "5.0.10")
+  if (comp >= 0) {
+    fslview_cmd = paste0(fslview_cmd, "_deprecated")
+  }
+  return(fslhelp(fslview_cmd))
 }
 
 #' @rdname fslview
