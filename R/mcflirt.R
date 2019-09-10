f4dafe8c52c212216627672ddcc1ba6ad726f3b6
muschellij2 <muschellij2@gmail.com>
2019-09-10 12:35:26 -0400

    updated the travis and mcflirt

--- R/mcflirt.R
+++ R/mcflirt.R
@@ -11,7 +11,7 @@
 #' @param verbose (logical) print out command before running
 #' @param ... additional arguments passed to \code{\link{readnii}}.
 #' @return If \code{retimg} then object of class nifti.  Otherwise,
-#' Result from system command, depends if intern is TRUE or FALSE.
+#' it will have additional attributes in the \code{additional_files} field.
 #' @export
 mcflirt = function(
   file,
@@ -25,6 +25,8 @@ mcflirt = function(
   
   outfile = check_outfile(outfile = outfile, retimg = retimg)
   opts = paste(opts, "-verbose", as.numeric(verbose), "-o")
+  file = checkimg(file)
+  file = path.expand(file)
   
   res = fslcmd(func = "mcflirt", 
                file = file,
@@ -38,7 +40,25 @@ mcflirt = function(
                frontopts = "-in",
                samefile = FALSE)
   
-  return(res)  
+  output_files = c(
+    mat = ".mat", 
+    par = ".par", 
+    abs_rms = "_abs.rms", 
+    abs_mean_rms = "_abs_mean.rms", 
+    rel_rms = "_rel.rms", 
+    rel_mean_rms = "_rel_mean.rms")
+  output_files = paste0(nii.stub(outfile), 
+                        output_files)
+  output_files = output_files[file.exists(output_files)]
+  if (retimg) {
+    outfile = readnii(outfile)
+  }
+  if (length(output_files) > 0) {
+    L = as.list(output_files)
+    names(L) = names(output_files)
+    attr(outfile, "additional_files") = L
+  }
+  return(outfile)  
 }
 
 #' @title MCFLIRT help
